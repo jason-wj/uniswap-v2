f1065c02f7535eea40511e15411b13fc066bd46c
ld <ld>
2022-11-23 17:48:31 +0800

    project init

---
+++ README.md
@@ -0,0 +1,12 @@
+# uniswap-v2
+基于hardhat框架管理和发布uniswap v2
+
+## 信息整理
+使用本项目，goerli测试网自行发布信息如下：
+1. [WETH9地址](https://goerli.etherscan.io/address/0xFe33eC9960E430608030e92860264B486Ae99Ef2) ：0xFe33eC9960E430608030e92860264B486Ae99Ef2
+2. [UniswapV2Factory合约地址](https://goerli.etherscan.io/address/0x08b99e6b892da793b3da07db14d83c86337d5b1c) ：0x08b99e6b892da793b3da07db14d83c86337d5b1c
+3. [UniswapV2Router02合约地址](https://goerli.etherscan.io/address/0x5484F49800869F92Cf5EdcAb711084D16D5c2fD7) ：0x5484F49800869F92Cf5EdcAb711084D16D5c2fD7
+
+
+## 发布
+npx hardhat --network goerli run scripts/deploy.js
---
+++ scripts/deploy.js
@@ -0,0 +1,91 @@
+// This is a script for deploying your contracts. You can adapt it to deploy
+// yours, or create new ones.
+
+const path = require("path");
+
+async function main() {
+    // This is just a convenience check
+    if (network.name === "hardhat") {
+        console.warn(
+            "You are trying to deploy a contract to the Hardhat Network, which" +
+            "gets automatically created and destroyed every time. Use the Hardhat" +
+            " option '--network localhost'"
+        );
+    }
+
+    // ethers is available in the global scope
+    const [deployer] = await ethers.getSigners();
+    console.log(
+        "Deploying the contracts with the account:",
+        await deployer.getAddress()
+    );
+
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
+    //solidity version：0.4.18
+    await weth9();
+
+    //solidity version：0.5.16
+    //await uniswapv2Factory();
+
+    //solidity version：0.6.6
+    //await uniswapV2Router02();
+
+    // We also save the contract's artifacts and address in the frontend directory
+    // saveFrontendFiles(token);
+}
+
+//solidity version：0.4.18
+async function weth9(){
+    const WETH9 = await ethers.getContractFactory("WETH9");
+    const weth9 = await WETH9.deploy();
+    await weth9.deployed();
+
+    console.log("WETH9 address:", weth9.address);
+}
+
+//solidity version：0.6.6
+async function uniswapV2Router02(){
+    const UniswapV2Router02 = await ethers.getContractFactory("UniswapV2Router02");
+    const uniswapV2Router02 = await UniswapV2Router02.deploy('0x08b99E6B892da793b3dA07db14D83c86337d5B1c','0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2');
+    await uniswapV2Router02.deployed();
+
+    console.log("UniswapV2Router02 address:", uniswapV2Router02.address);
+}
+
+//solidity version：0.5.16
+async function uniswapv2Factory(){
+    const UniswapV2Factory = await ethers.getContractFactory("UniswapV2Factory");
+    const uniswapV2Factory = await UniswapV2Factory.deploy('0xE5e69B292170459a4e4CC77f94491681fF1f1636');
+    await uniswapV2Factory.deployed();
+
+    console.log("UniswapV2Factory address:", uniswapV2Factory.address);
+}
+
+function saveFrontendFiles(token) {
+    const fs = require("fs");
+    const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts");
+
+    if (!fs.existsSync(contractsDir)) {
+        fs.mkdirSync(contractsDir);
+    }
+
+    fs.writeFileSync(
+        path.join(contractsDir, "contract-address.json"),
+        JSON.stringify({Token: token.address}, undefined, 2)
+    );
+
+    const TokenArtifact = artifacts.readArtifactSync("Token");
+
+    fs.writeFileSync(
+        path.join(contractsDir, "Token.json"),
+        JSON.stringify(TokenArtifact, null, 2)
+    );
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
